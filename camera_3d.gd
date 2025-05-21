88a3abefc006bb046b0c496b06254de4af6ae0cc
haha64142 <paul.w64142@gmail.com>
2025-05-21 01:50:18 -0500

    -Camera now moves with the mouse proporly -Camera only moves when the cursor is a crosshair

--- camera_3d.gd
+++ camera_3d.gd
@@ -4,9 +4,12 @@ var vector_diff := Vector2(0, 0)
 var center := Vector2(ProjectSettings.get_setting("display/window/size/viewport_width") / 2, ProjectSettings.get_setting("display/window/size/viewport_height") / 2)
 
 func _ready() -> void:
-	position = Vector3(0, 6, 8)
+	position = Vector3(0, 6, 6)
 
 
 func _process(_delta: float) -> void:
-	vector_diff = Global.mouse_pos - center
-	position = Vector3(vector_diff.x / 137.14, 6, vector_diff.y / 33.57)
+	if !Global.mouse_visible:
+		vector_diff = Global.mouse_pos - center
+		position = Vector3(vector_diff.x / 137.14, 6, vector_diff.y / 33.57 + 6)
+	else:
+		position = Vector3(0, 6, 6)
--- global.gd
+++ global.gd
@@ -2,3 +2,4 @@ extends Node
 
 var mouse_pos := Vector2(0, 0)
 var respawn_pos := Vector3(0, 2, 0)
+var mouse_visible := false
