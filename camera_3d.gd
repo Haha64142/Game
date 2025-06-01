176a1a0441c968166315b9f6f442cbcb097253c8
Haha64142 <paul.w64142@gmail.com>
2025-06-01 17:38:53 -0500

    Change player and camera size to prevent camera unitened culling
    Before this change, the tiles on the bottom of the screen would sometimes not render. I fixed this by decreasing the player and tile sizes.
    
    Also renamed mesh_lib to tile_mesh

--- camera_3d.gd
+++ camera_3d.gd
@@ -4,12 +4,12 @@ var vector_diff := Vector2(0, 0)
 var center := Vector2(ProjectSettings.get_setting("display/window/size/viewport_width") / 2, ProjectSettings.get_setting("display/window/size/viewport_height") / 2)
 
 func _ready() -> void:
-	position = Vector3(0, 6, 7)
+	position = Vector3(0, 6.5, 7)
 
 
 func _process(_delta: float) -> void:
 	if !Global.mouse_visible:
 		vector_diff = Global.mouse_pos - center
-		position = Vector3(vector_diff.x / 109.5, 6, vector_diff.y / 78 + 7)
+		position = Vector3(vector_diff.x / 153, 6.5, vector_diff.y / 110 + 7)
 	else:
-		position = Vector3(0, 6, 7)
+		position = Vector3(0, 6.5, 7)
