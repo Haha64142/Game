16e45654470a918582135be22ab7dcb7f29c3ff1
Haha64142 <paul.w64142@gmail.com>
2025-06-18 18:32:27 -0500

    Add an Enemy node class
    Add an Enemy class that inherits from Area3D. It handles being hitting and being hit by the player. It can also show which objects it's currently colliding with.
    The test enemy now moves in the direction that the arrow was going.
    Also made the arrow delete itself when it hits a wall or goes to far from the center
    Also formatted the code to match the Godot style guide

--- arena.gd
+++ arena.gd
@@ -1,11 +1,22 @@
 extends Node3D
 
-@export var arrow_scene : PackedScene
- 
-func shoot_arrow(shot_power: float, mouse_pos: Vector2, player_pos: Vector3):
-	var arrow : Node = arrow_scene.instantiate()
-	add_child(arrow)
-	arrow.shoot(shot_power, mouse_pos, player_pos)
+@export var arrow_scene: PackedScene
+
+func _process(_delta: float) -> void:
+	var collision_keys := ["Floor", "Wall", "Player", "Attack1", "Arrow"]
+	var _output := ""
+	for i in collision_keys:
+		if i != "Floor":
+			_output += ", "
+		_output += i + ": " + ("🟩" if $Enemy.collisions[i] else "🟥")
+	
+	#print(_output)
+
+
+func _on_player_arrow_fired(shot_power: float, mouse_pos: Vector2, player_pos: Vector3):
+	var Arrow: Area3D = arrow_scene.instantiate()
+	add_child(Arrow)
+	Arrow.shoot(shot_power, mouse_pos, player_pos)
 
 
 func _on_player_attack_1_finished() -> void:
