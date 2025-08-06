68594b15524ef7b6a3aa31d4564f84ff99ad6bfa
Haha64142 <paul.w64142@gmail.com>
2025-08-06 14:42:16 -0500

    Rebake nav mesh
    This was done to allow the enemy to reach the player when he's on the wall

--- global.gd
+++ global.gd
@@ -10,7 +10,7 @@ enum AttackType {
 }
 
 var mouse_pos := Vector2.ZERO
-var respawn_pos := Vector3(0, 3, 0)
+var respawn_pos := Vector3(0, 1, 0)
 var mouse_visible := false
 var player_pos := Vector3.ZERO
 
