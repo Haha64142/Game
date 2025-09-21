9a7b9c0e0804278f0ca0e6dce30001ce0a64d419
Haha64142 <paul.w64142@gmail.com>
2025-09-21 18:12:37 -0500

    Continue work on the settings menu
    The SettingsScrollBar is almost complete. I still need to make it update values
    in the global Settings node

--- main_menu.gd
+++ main_menu.gd
@@ -10,3 +10,7 @@ func _on_play_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings_screen.tscn")
