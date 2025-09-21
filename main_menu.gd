2d5d287efc57e693aeafa04e3f4c8836c2c895d4
Haha64142 <paul.w64142@gmail.com>
2025-09-20 20:33:06 -0500

    Add PrimaryButton type variation to theme
    Add exit button to main menu

--- main_menu.gd
+++ main_menu.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://arena.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
